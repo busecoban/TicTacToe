f5f901397565629122d023263de02dd16ad2422c
Buse Çoban <busecoban48@icloud.com>
2023-03-14 14:54:12 +0300

    check dioganal win and draw winner path

--- TicTacToeAPI.java
+++ TicTacToeAPI.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 public class TicTacToeAPI {
     private String player1,player2;
     private String winType;
@@ -17,7 +18,7 @@ public class TicTacToeAPI {
         turn=player2;
         }
         else{
-        turn=player2;
+        turn=player1;
         }
         return turn;
 
@@ -53,19 +54,76 @@ public class TicTacToeAPI {
         !(board[1][i].getPiece().getValue().equals(" "))&&
         !(board[2][i].getPiece().getValue().equals(" "))){
 
-            winType="rows";
+            winType="row";
             return board[0][i];
         }
     }
 
     //check diogonal wins
+    if(board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0])
+    && board[0][2].getPiece()!=null){
+        winType="diogonal";
+        return board[0][2];
+    }
+
+    if(board[0][0].equals(board[2][2]) && board[0][0].equals(board[2][0])
+    && board[0][2].getPiece()!=null){
+        winType="diogonal";
+        return board[0][0];
+    }
+    return null;
     
 
 
 
 
-return null;
     }
+
+    public void drawWinPath(Graphics G,Square winner,int squareLength){
+        G.setColor(Color.CYAN);
+        ((Graphics2D)G).setStroke(new BasicStroke(8));
+
+        if(winType.equals("diogonal")){
+
+            if(winner.getCol()-1==0){
+                G.drawLine(
+                (winner.getCol()-1)*squareLength+100,
+                (winner.getRow()-1)*squareLength+90,
+                squareLength*3+100,
+                squareLength*3+90);
+
+            }else if(winner.getCol()-2==2){
+                G.drawLine((winner.getCol()-1)*squareLength+100+squareLength,
+                (winner.getRow()-1)*squareLength+90,
+                100,
+                squareLength*3+90);
+            }
+
+
+        }else if(winType.equals("row")){
+            G.drawLine((winner.getRow()-1*squareLength)+100,
+            (winner.getCol()-1)*squareLength+200,
+            squareLength*3+100,
+            (winner.getCol()-1)*squareLength+200);
+
+
+        }else if(winType.equals("column")){
+            G.drawLine((winner.getRow()-1)*squareLength+200,
+            (winner.getCol()-1)*squareLength+90,
+            (winner.getRow()-1)*squareLength+200,
+            squareLength*3+90);
+        }
+    }
+
+
+    /* 
+    public void gameResult(Graphics G,String result){
+        G.setFont(new Font("Arial",Font.BOLD,75));
+        if(result.equals("TIE")){
+
+        }
+    }
+    */
 
 
 
